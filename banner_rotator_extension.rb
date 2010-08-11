72eb0aeca6067ee7656f52d323680351506e5e09
Andrew vonderLuft <avonderluft@avlux.net>
2010-08-11 15:57:13 -0700

    update version and URL

--- banner_rotator_extension.rb
+++ banner_rotator_extension.rb
@@ -1,7 +1,7 @@
 class BannerRotatorExtension < Radiant::Extension
-  version "1.1"
+  version "0.7.1"
   description "Allows addition and independent management of rotating banners on pages."
-  url "http://github.com/seancribbs/radiant-banner-rotator-extension"
+  url "http://github.com/avonderluft/radiant-banner-rotator-extension"
   
   define_routes do |map|
     map.namespace :admin do |admin|
